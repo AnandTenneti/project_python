f2ae530bf7f5e3eaacb079f4fab5361627005dbd
AnnadTenneti <tenneti.anand@gmail.com>
2024-08-04 23:51:37 +0530

    first python file using range function uploaded

---
+++ sum_of_even.py
@@ -0,0 +1,12 @@
+print("Approach 1")
+sum_of_even_numbers=0
+for i in range(2,101,2):
+    sum_of_even_numbers +=i  
+print("Approach 1",sum_of_even_numbers)
+
+print("Approach 2")
+sum_of_even_nos=0
+for i in range(1,101):
+    if i%2==0:
+        sum_of_even_nos +=i  
+print("Approach 2",sum_of_even_nos)
